733ebfbe415f6af1fd767a7da8b6f59ad70444de
RodrigoNS <rodrigoons@gmail.com>
2024-05-26 14:32:14 -0300

    criado menu editar/delatar nos posts

--- src/components/cards/Post.jsx
+++ src/components/cards/Post.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 import moment from "moment"
 
+import Menu from "../navigation/Menu"
+
 const PostContainer = styled.div`
   background-color: ${props => props.theme.white};
   padding: 20px 40px;
@@ -25,9 +27,36 @@ const TextContainer = styled.p`
   margin-top: 10px;
 `
 
+const MenuContainer = styled.div`
+  float: right
+`
+
 function Post ({ user, date, text }) {
+  
+  const handlePostEdit = () => {
+    console.log("HANDLE POST EDIT")
+  }
+
+  const handlePostDelete = () => {
+    console.log("HANDLE POST DELETE")
+  }
+  
   return (
     <PostContainer>
+      <MenuContainer>
+        <Menu 
+          options={[
+            {
+              text: 'Editar',
+              onClick: handlePostEdit
+            },
+            {
+              text: 'Deletar',
+              onClick: handlePostDelete
+            }
+          ]} 
+        />
+      </MenuContainer>
       <StyledUsername> @{user} </StyledUsername>
       <StyledDate> {moment(date).format('LLL')} </StyledDate>
       <TextContainer> {text} </TextContainer>
---
+++ src/components/navigation/Menu.jsx
@@ -0,0 +1,71 @@
+import { useState, useRef, useEffect } from "react"
+import styled from "styled-components"
+
+const Dots = styled.img`
+  cursor: pointer;
+`
+
+const StyledMenu = styled.div`
+  width: 100px;
+  background-color: ${props => props.theme.white};
+  box-shadow: 6px 5px 15px 5px rgba(0, 0, 0, 0.25);
+  position: absolute;
+  right: 0;
+  border-radius: 3px;
+  padding: 5px;
+
+  display: ${props => props.show ? 'block' : 'none'};
+`
+
+const StyledOption = styled.div`
+  padding: 12px;
+  cursor: pointer;
+  font-size: 15px;
+
+  :hover {
+    background-color: ${props => props.theme.inputBackground}
+  }
+`
+
+const StyledMenuContainer = styled.div`
+  position: relative;
+`
+
+const Menu = ({ options }) => {
+  const [show, setShow] = useState(false)
+  const menuRef = useRef(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShow(false)
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside, true)
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true)
+    }
+  }, [menuRef])
+
+  return (
+    <StyledMenuContainer>
+      <Dots src="/three-dots.svg" height="20px" onClick={() => setShow(!show)} />
+      <StyledMenu show={show} ref={menuRef} onBlur={() => setShow(false)} >
+        {
+          options.map((option, pos) => 
+            <StyledOption 
+              key={`menu-option-${pos}`}
+              onClick={option.onClick}
+            > 
+              {option.text} 
+            </StyledOption>
+          )
+        }
+      </StyledMenu>
+    </StyledMenuContainer>
+  )
+}
+
+export default Menu
